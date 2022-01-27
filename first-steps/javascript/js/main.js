f8ca538855ded2ed490ddab43e3d3d9a3e5d3d29
AltherLago <ajsl@ic.ufal.br>
2022-01-27 08:56:28 -0300

    js

---
+++ first-steps/javascript/js/main.js
@@ -0,0 +1,34 @@
+
+/*
+// class1
+
+var nome = "Alther Lago";
+var idade = 26;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos");
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão",  "Brasil"))
+console.log(frase.toUpperCase());
+*/
+
+// class 2
+
+/*
+var lista = ["maça", "pêra", "laranja"];
+lista.push("uva");
+lista.pop();
+console.log(lista);
+console.log(lista[1]);
+console.log(lista.length);
+console.log(lista.reverse);
+console.log(lista.toString());
+console.log(lista.toString()[0]);
+console.log(lista.join(" - "));
+*/
+
+var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}];
+console.log(frutas);
+
