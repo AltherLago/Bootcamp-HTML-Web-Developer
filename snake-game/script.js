a4e5b3a2e814738d9b67d151492a7c46bd1d83a4
AltherLago <ajsl@ic.ufal.br>
2022-02-17 12:34:48 -0300

    stop game

--- snake-game/script.js
+++ snake-game/script.js
@@ -46,7 +46,13 @@ function startGame(){
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
-    
+   
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(game);
+        }
+    }
+
     createBG();
     createSnake();
     createFood(); 
@@ -66,15 +72,12 @@ function startGame(){
         food.y = Math.floor(Math.random() * 15 + 1) * box
     }
 
-
     let newHead = {
         x: snakeX,
         y: snakeY
     }
 
-
     snake.unshift(newHead);
-    
 }
 
 let game = setInterval(startGame, 100);
